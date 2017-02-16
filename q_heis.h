3ff7132793ef795ff04fafc02958f82d338e8729
kristeey <krss@stud.ntnu.no>
2017-02-16 19:12:27 +0100

    Add files via upload

--- q_heis.h
+++ q_heis.h
@@ -1,14 +1,18 @@
 
 
-void q_init();
-
 void q_set_request(int floorDir);
 
-int q_get_next_floor(int floor, int dir);
+int q_get_next_floor(int last_floor, int last_dir);
 
-int q_get_next_direction(int floor, int dir);
+int q_get_next_direction(int last_floor, int last_dir);
 
 void q_clear_order(int floor);
 
 void q_clear_queue();
+
+int q_floor_and_dir_to_array_int(int floor, int dir);
+
+int q_array_int_to_floor(int array_int);
+
+//int q_array_int_to_dir(int array_int);
 
