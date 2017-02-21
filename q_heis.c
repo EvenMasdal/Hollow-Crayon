b9032ca0988a14a2d8b8e4240ae0f4ea7f400ae7
kristeey <krss@stud.ntnu.no>
2017-02-21 23:52:04 +0100

    Add files via upload

--- q_heis.c
+++ q_heis.c
@@ -1,6 +1,7 @@
 #include "q_heis.h"
 #include <stdio.h>
 
+
 static int q_number_of_floors = 4;
 
 static int q_array_size = (q_number_of_floors * 2) - 2 ;
@@ -20,8 +21,8 @@ void q_set_request(int floorDir){
 //henter neste 1'er i køen, BØR TENKE PÅ FEIL DERSOM LAST_DIR = 0!!!!!!!
 int q_get_next_floor(int last_floor, int last_dir){
 
-	int arr_i = q_array_int_to_floor(last_floor, last_dir);
-	int start_i = q_array_int_to_floor(last_floor, last_dir);
+	int arr_i = q_floor_and_dir_to_array_int(last_floor, last_dir);
+	int start_i = q_floor_and_dir_to_array_int(last_floor, last_dir);
 
 	while (true){
 		arr_i = arr_i +1; 			//starter på neste etasje
@@ -68,7 +69,7 @@ void q_clear_order(int floorDir){
 /*______________________________________________________________________________________________________________________________________________________*/
 
 //setter alle element i lista til 0
-void q_clear_queue(){
+void q_clear_queue(void){
 	for (element = 0; element < q_array_size ; element = element + 1){
 		queue_arr[element] = 0;
 	}
@@ -96,6 +97,8 @@ int q_floor_and_dir_to_array_int(int floor, int dir){
 		case 3: 
 			if (dir != 1 || dir != ){return -1;}
 			return 3;
+		default:
+			return -1;
 	}
 }
 
@@ -116,5 +119,7 @@ int q_array_int_to_floor(int array_int){
 			return 2;
 		case 5:
 			return 1;
+		default:
+			return -1;
 	}
 }
