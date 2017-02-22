b67fab1d0b6b7ffa3019b90e070111c9d196f7da
EvenMasdal <EvenMasdal@users.noreply.github.com>
2017-02-22 19:40:37 +0100

    Update timer_heis.c

--- timer_heis.c
+++ timer_heis.c
@@ -1,16 +1,26 @@
 #include "timer_heis.h"
 #include <stdio.h>
+#include <time.h>
 
-static bool timer_status;
+static time_t seconds
 
-void timer_start(int time){
+void timer_start(){
 
-}
-
-void timer_stop(void){
-
+  seconds = time(NULL);
+  
+  return(0);
 }
 
 bool timer_get_status(void){
+  return (time(NULL) - seconds) > 3;
+}
 
+main(){
+  timer_start()
+  int b;
+  for(int i = 0; i<10000; i+=1){
+      b = 1 + 1;
+  }
+  timer_get_status();
+  
 }
