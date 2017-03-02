408297660b99ffb81c3dc0f63884b23d6523c455
EvenMasdal <even.masdal@gmail.com>
2017-03-02 17:22:03 +0100

    fiksa

--- main.c
+++ main.c
@@ -1,32 +1,20 @@
 #include "elev.h"
+#include "ctrl.h"
 #include <stdio.h>
 
-
 int main() {
-    // Initialize hardware
-    if (!elev_init()) {
-        printf("Unable to initialize elevator hardware!\n");
-        return 1;
-    }
+    ctrl_init();
 
-    printf("Press STOP button to stop elevator and exit program.\n");
-
-    elev_set_motor_direction(DIRN_UP);
-
-    while (1) {
-        // Change direction when we reach top/bottom floor
-        if (elev_get_floor_sensor_signal() == N_FLOORS - 1) {
-            elev_set_motor_direction(DIRN_DOWN);
-        } else if (elev_get_floor_sensor_signal() == 0) {
-            elev_set_motor_direction(DIRN_UP);
+    while (1){
+        if (elev_get_stop_signal()){
+            ctrl_emergency_stop();
         }
-
-        // Stop elevator and exit program if the stop button is pressed
-        if (elev_get_stop_signal()) {
-            elev_set_motor_direction(DIRN_STOP);
-            break;
+        ctrl_request();
+        if (moving == 0){
+            ctrl_move();
+        }
+        if (elev_get_floor_sensor() != -1){
+            ctrl_floor_sensor();
         }
     }
-
-    return 0;
 }
