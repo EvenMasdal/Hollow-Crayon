2a841c98be219c2f596bd7b1d9bbf6e641ae5e35
EvenMasdal <EvenMasdal@users.noreply.github.com>
2017-03-02 16:47:38 +0100

    Lastet opp heisdriver

--- main.c
+++ main.c
@@ -1,21 +1,32 @@
 #include "elev.h"
-#include "ctrl.h"
 #include <stdio.h>
 
 
 int main() {
-    ctrl_init();
+    // Initialize hardware
+    if (!elev_init()) {
+        printf("Unable to initialize elevator hardware!\n");
+        return 1;
+    }
 
-    while (1){
-        if (elev_get_stop_signal()){
-            ctrl_emergency_stop();
+    printf("Press STOP button to stop elevator and exit program.\n");
+
+    elev_set_motor_direction(DIRN_UP);
+
+    while (1) {
+        // Change direction when we reach top/bottom floor
+        if (elev_get_floor_sensor_signal() == N_FLOORS - 1) {
+            elev_set_motor_direction(DIRN_DOWN);
+        } else if (elev_get_floor_sensor_signal() == 0) {
+            elev_set_motor_direction(DIRN_UP);
         }
-        ctrl_request();
-        if (moving == 0){
-            ctrl_move();
-        }
-        if (elev_get_floor_sensor() != -1){
-            ctrl_floor_sensor();
+
+        // Stop elevator and exit program if the stop button is pressed
+        if (elev_get_stop_signal()) {
+            elev_set_motor_direction(DIRN_STOP);
+            break;
         }
     }
+
+    return 0;
 }
